49f909fdd3533d97ff2a974ceb4bbd39989284b7
Andras Gyori <gandras@cloudera.com>
2020-11-20 19:23:47 +0100

    Implement sender test client

--- src/bin/test_client.rs
+++ src/bin/test_client.rs
@@ -6,10 +6,11 @@ use sha2::Digest;
 use bytes::BytesMut;
 use fordragon_backend::net::protocol::opcode::NetworkRecvOpCode;
 use fordragon_backend::net::protocol::encode::{BBEncodable, ByteEncoder};
-use std::io::BufReader;
+use std::io::{BufReader, stdin, BufRead};
 use tokio::io;
-use tokio::prelude::{Future, Stream};
 use tokio::fs;
+use std::str::FromStr;
+use std::convert::TryFrom;
 
 const LOCALHOST: &str = "127.0.0.1";
 const PORT: &str = "47331";
@@ -18,24 +19,47 @@ const PORT: &str = "47331";
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = format!("{}:{}", LOCALHOST, PORT).parse::<SocketAddr>()?;
     let mut stream = TcpStream::connect(&addr).await?;
-    // let mut temp_hash = sha2::Sha256::new();
-    // let encoder = ByteEncoder::new();
-    // temp_hash.update(&format!("admin:admin:{}", "secret12345").as_bytes());
-    // let hash = format!("{:x}", &temp_hash.finalize());
-    // let user = "admin".to_string();
-    // let mut bytes_vec = Vec::new();
-    // let auth_bytes = &(NetworkRecvOpCode::AUTH as u16).to_le_bytes();
-    // println!("{:#?}", auth_bytes);
-    // bytes_vec.extend(auth_bytes);
-    // bytes_vec.extend(encoder.encode_str("admin"));
-    // bytes_vec.extend(encoder.encode_str(hash.as_str()));
-    //
-    // println!("{:#?}", bytes_vec.as_slice());
+    let encoder = ByteEncoder::new();
 
-    let stdin = io::stdin();
-    let reader = BufReader::new
-    stream.write_all(bytes_vec.as_slice()).await?;
-    stream.flush().await?;
+    println!(r#"Line prefixes are:
+    ptc: Protocol Type
+    str: String encoding
+    u16: 2 bytes number
+    u32: 4 bytes number"#);
+    let mut vec_byte : Vec<u8> = Vec::new();
+    let mut stored_lines = Vec::new();
+
+    for line in  stdin().lock().lines() {
+       let line = line?;
+        if line.len() < 3 {
+            panic!("Invalid prefix");
+        }
+        if line.contains("send") {
+            println!("Message sent");
+            println!("{:?}", vec_byte.as_slice());
+
+            stream.write_all(vec_byte.as_slice()).await?;
+            stream.flush().await?;
+            vec_byte = Vec::new();
+        }
+
+
+        match &line.as_str()[0..3] {
+            "ptc" => {
+                let op = u16::from_str(&line.as_str()[3..]).expect("Invalid op code number");
+                let op_code = NetworkRecvOpCode::try_from(op).expect("Invalid op code");
+                vec_byte.extend(op_code.encode_as_bbp())
+            }
+
+            "str" => vec_byte.extend(encoder.encode_str(&line.as_str()[3..])),
+            "u16" => vec_byte.extend_from_slice(&u16::from_str(&line.as_str()[3..]).unwrap().to_le_bytes()),
+            "u32" => vec_byte.extend_from_slice(&u32::from_str(&line.as_str()[3..]).unwrap().to_le_bytes()),
+            _ => return panic!("Invalid prefix, use str/u16/u32")
+        }
+        let line_without_prefix = &line.as_str()[3..];
+        stored_lines.push(line_without_prefix.to_string());
+        println!("Stored lines {:?}", stored_lines);
+    }
 
     Ok(())
 }
